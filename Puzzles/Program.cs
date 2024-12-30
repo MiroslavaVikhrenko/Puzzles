dc01c2194578d09384ae8362efc6be8b504797a9
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-30 14:32:14 -0700

    Solve puzzle

--- Puzzles/Program.cs
+++ Puzzles/Program.cs
@@ -2,6 +2,18 @@
 {
     internal class Program
     {
+        /*
+         OUTPUT:
+
+        In B.B()
+        In D.Func(). Message is original value
+        In D.Func(). Message is I've been sent again
+
+        KEY POINT:
+        All members of an object, no matter where in the inheritance hierarchy, are initialized
+        before any constructors execute. (starts at line private readonly string msg = "original value";)
+        
+         */
         static void Main(string[] args)
         {
             var d = new D();
