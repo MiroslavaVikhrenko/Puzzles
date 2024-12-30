0caee764a5ec07c293fd924f2d7a610caad0818b
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-30 14:21:51 -0700

    Add project files.

---
+++ Puzzles/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Puzzles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new D();
+            d.Func();
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class B
+    {
+        public abstract void Func();
+        protected B()
+        {
+            Console.WriteLine("In B.B()");
+            Func();
+        }
+    }
+
+    public class D : B
+    {
+        private readonly string msg = "original value";
+        public D()
+        {
+            msg = "I've been sent again";
+        }
+        public override void Func()
+        {
+            Console.WriteLine($"In D.Func(). Message is {msg}");
+        }
+    }
+}
