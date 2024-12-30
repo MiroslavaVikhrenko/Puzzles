9fbf8fe092a92ef31e6cb36bca2710247d4b550b
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-30 14:42:10 -0700

    Imitate a System.InvalidCastException scenario

---
+++ Puzzle2_GenericsConversions/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Puzzle2_GenericsConversions
+{
+    internal class Program
+    {
+        /*
+        > Understand C# rules on generic type parameters
+        > Distinguish compile time and run time types
+
+        original code results in "System.InvalidCastException: 
+        'Unable to cast object of type 'System.Int32' to type 'System.Double'.'"
+         */
+        static void Main(string[] args)
+        {
+            var nums = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var doubles = from double n in nums select n;
+            foreach (var d2 in doubles)
+                Console.WriteLine(d2);
+            Console.ReadKey();
+        }
+    }
+}
