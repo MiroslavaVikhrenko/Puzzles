8901f1c1694104ee0534973c836c3c97a81d629c
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-30 17:59:03 -0700

    Set up Default_Parameters_and_Overrides puzzle

---
+++ Puzzle11_Default_Parameters_and_Overrides/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Puzzle11_Default_Parameters_and_Overrides
+{
+    internal class Program
+    {
+        /*
+         > Understand how overloads affect default parameters
+         > Understand how method resolution affects parameters
+         
+         Original code output:
+         Welcome to our restaurant, Diner
+         */
+        static void Main(string[] args)
+        {
+            Waiter greeter = new Waiter();
+            
+            var result = greeter.SayHello();
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+    public interface IGreeter
+    {
+        string SayHello(string name = "Guest");
+    }
+    public class Waiter : IGreeter
+    {
+        public string SayHello(string name = "Diner")
+        {
+            return string.Format("Welcome to our restaurant, {0}", name);
+        }
+    }
+}
