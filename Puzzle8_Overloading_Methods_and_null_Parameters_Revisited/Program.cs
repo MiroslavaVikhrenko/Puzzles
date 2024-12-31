6f07f6a6f0ced2eb09966af57baa02e6547d1804
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-30 17:23:23 -0700

    Solve Overloading_Methods_and_null_Parameters_Revisited puzzle

--- Puzzle8_Overloading_Methods_and_null_Parameters_Revisited/Program.cs
+++ Puzzle8_Overloading_Methods_and_null_Parameters_Revisited/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Puzzle8_Overloading_Methods_and_null_Parameters_Revisited
+﻿using System;
+
+namespace Puzzle8_Overloading_Methods_and_null_Parameters_Revisited
 {
     internal class Program
     {
@@ -8,12 +10,19 @@
 
          Original code output:
          calling string version
+
+         KEY POINTS:
+         > If types are NOT related by inheritance, null becomes ambiguous
+         > Default(type) really is your friend
          */
         static void Main(string[] args)
         {
             //ver 1 calling string version
             var target = new TestClass();
-            target.Test(null);
+            //target.Test(null);
+
+            //ver 2 - fix | calling IEnumerable version
+            target.Test(default(IEnumerable<int>));
 
             Console.ReadKey();
         }
@@ -25,10 +34,29 @@
         {
             Console.WriteLine("calling string version");
         }
-        public void Test(IEnumerable<char> parm)
+
+        //ver 1 calling string version
+        //public void Test(IEnumerable<char> parm)
+        //{
+        //    Console.WriteLine("calling IEnumerable version");
+        //}
+
+
+        //ver 2 cannot compile 
+        //if we add the below method - it does not compile because of ambiguity 
+        //CS0121 The call is ambiguous between the following methods or properties:
+        //'TestClass.Test(string)' and 'TestClass.Test(IEnumerable<int>)'	
+
+        public void Test(IEnumerable<int> parm)
         {
             Console.WriteLine("calling IEnumerable version");
         }
+
+        //ver 3 calling string version
+        //public void Test<T>(IEnumerable<T> parm)
+        //{
+        //    Console.WriteLine("calling IEnumerable version");
+        //}
         public void Test(object param)
         {
             Console.WriteLine("calling object version");
