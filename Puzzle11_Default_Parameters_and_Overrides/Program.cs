85796c9a2f1325da9fb39e5966aeaac8f55e9ba9
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-30 18:07:05 -0700

    Solve Default_Parameters_and_Overrides puzzle

--- Puzzle11_Default_Parameters_and_Overrides/Program.cs
+++ Puzzle11_Default_Parameters_and_Overrides/Program.cs
@@ -8,11 +8,28 @@
          
          Original code output:
          Welcome to our restaurant, Diner
+
+         KEY POINTS:
+         > When the compiler picks a method overload, that determines the value of any default parameters
+         > Explicit interface implementations can never be picked
+         > Static types of variable determines which method
+         > Default values should match for all overloads
          */
         static void Main(string[] args)
         {
-            Waiter greeter = new Waiter();
-            
+            //ver 1 Welcome to our restaurant, Diner
+            //Waiter greeter = new Waiter();
+
+            //ver 2 Welcome to our restaurant, Diner
+            //var greeter = new Waiter();
+
+            //ver 3 Welcome to our restaurant, Guest
+            //still calling method from class, but taking par from interface
+            //IGreeter greeter = new Waiter();
+
+            //ver 4 Welcome to our restaurant, Diner
+            dynamic greeter = new Waiter();
+
             var result = greeter.SayHello();
             Console.WriteLine(result);
 
