9dd821769f48a3421a042f93a67e72f03cfd5d6b
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-30 18:14:08 -0700

    Set up Evaluation_of_Named_Parameters puzzle

---
+++ Puzzle12_Evaluation_of_Named_Parameters/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Puzzle12_Evaluation_of_Named_Parameters
+{
+    internal class Program
+    {
+        /*
+         > Understand evaluation of named parameters
+         > Learn strategies for consistency in named parameters
+
+         Original code output:
+         CalcDenominator
+         CalcNumerator
+         2
+         */
+        static void Main(string[] args)
+        {
+            var answer = divide(y : CalcDenominator(), x: CalcNumerator());
+            Console.WriteLine(answer);
+            Console.ReadKey();
+        }
+
+        static int CalcDenominator() { Console.WriteLine("CalcDenominator"); return 100; }
+        static int CalcNumerator() { Console.WriteLine("CalcNumerator"); return 200; }
+        static int divide(int x, int y) { return x / y; }
+
+    }
+}
