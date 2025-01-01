9053758d68c12193f3bbd3fa4dca7db3449600f5
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-01-01 15:42:09 -0700

    Solve Bound_Variables puzzle

--- Puzzle13_Bound_Variables/Program.cs
+++ Puzzle13_Bound_Variables/Program.cs
@@ -6,30 +6,12 @@
          > Understand when lambda expressions are evaluation
          > Understand bound variables and closures
 
-         Original code output:
-         15
-         16
-         17
-         18
-         19
-         20
-         21
-         22
-         23
-         24
-         25
-         26
-         27
-         28
-         29
-         30
-         31
-         32
-         33
-         34
+         Original code output: 15 16 17 18 ... 33 34
          
          KEY POINTS:
-         >
+         > Variables in lambda expressions are evaluated when the lambda is executed, NOT when it's defined.
+         > Changes to local variables are observed when the lambda is executed.
+         > Avoid lambdas with side-effects.
          */
         static void Main(string[] args)
         {
@@ -38,9 +20,25 @@
             Func<IEnumerable<int>> numbers =
                 () => Enumerable.Range(start, count);
 
+            //ver 3 | output: 0 1 2 3 4
+            var listOfNumbers = numbers().ToList();
+
+            //ver 2 | output: 10
+            var sum = numbers().Sum();
+
+            //ver 1 | output: 15 16 17 18 ... 33 34
             start = 15;
             count = 20;
             numbers().ForAll(item => Console.WriteLine(item));
+
+            //ver 2 | output: 10
+            Console.WriteLine();
+            Console.WriteLine(sum);
+
+            //ver 3 | output: 0 1 2 3 4
+            Console.WriteLine();
+            listOfNumbers.ForAll(item => Console.WriteLine(item));
+
             Console.ReadKey();
         }        
     }
