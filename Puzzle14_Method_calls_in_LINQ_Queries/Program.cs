25590b455762ad8a59beaccc2e04a656a90e7c9a
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-01-02 17:23:37 -0700

    Set up Value_type_semantics puzzle

---
+++ Puzzle14_Method_calls_in_LINQ_Queries/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Puzzle14_Method_calls_in_LINQ_Queries
+{
+    internal class Program
+    {
+        /*
+         > Understand when lambda expressions are evaluation
+         > Understand bound variables and closures
+
+         Original code output: 
+         
+         KEY POINTS:
+         > .
+         */
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Puzzle14_Value_type_semantics/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Puzzle14_Value_type_semantics
+{
+    internal class Program
+    {
+        /*
+         > Understand the more subtle differences between reference semantics and value semantics
+         > Understand why the recommendation for immutable value types exists
+
+         Original code output:
+         Makoto
+         Ken
+         */
+        static void Main(string[] args)
+        {
+            var peeps = new List<Person>
+            {
+                new Person{FirstName = "Makoto", LastName = "Yamamoto"},
+                new Person{FirstName = "Taro", LastName = "Sato"}
+            };
+
+            var person = peeps[0];
+            person.FirstName = "Ken";
+
+            //ver 1 | output: Makoto Ken
+            Console.WriteLine(peeps[0].FirstName); 
+            Console.WriteLine(person.FirstName);
+
+
+            Console.ReadKey();
+        }
+    }
+
+    public struct Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
