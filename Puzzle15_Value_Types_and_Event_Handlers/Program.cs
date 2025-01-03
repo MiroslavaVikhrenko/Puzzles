12c5f7da7aa5fdbe5fe62f377f7a00a699de7344
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-01-02 17:55:20 -0700

    Solve Value_Types_and_Event_Handlers puzzle

--- Puzzle15_Value_Types_and_Event_Handlers/Program.cs
+++ Puzzle15_Value_Types_and_Event_Handlers/Program.cs
@@ -8,9 +8,14 @@ namespace Puzzle15_Value_Types_and_Event_Handlers
         /*
          > Understand the more subtle differences between reference semantics and value semantics
          > Understand lifetimes of event handlers and value types      
+         
+         Original code output: 
+         Current count: 1
 
          KEY POINTS:
-         > 
+         > Assignments makes a copy of a value type
+         > Event Handler Assignment must assign a variable to the target object (if method is an instance method)
+         > Avoid using value types to handle events
          */
         static void Main(string[] args)
         {
@@ -18,8 +23,14 @@ namespace Puzzle15_Value_Types_and_Event_Handlers
 
             var mngr = new Timer();
 
+            //ver 1 and 2
+            //mngr.Interval = 500;
+            //mngr.Elapsed += new ElapsedEventHandler(underTest.TimerEvent);
+            //mngr.Start();
+
+            //ver 3 (with struct) | count changes
             mngr.Interval = 500;
-            mngr.Elapsed += new ElapsedEventHandler(underTest.TimerEvent);
+            mngr.Elapsed += (_, _) => underTest.EchoCount();
             mngr.Start();
 
             Console.ReadLine();
@@ -30,7 +41,7 @@ namespace Puzzle15_Value_Types_and_Event_Handlers
             Console.ReadKey();
         }
     }
-
+    //ver 1 | Current count: 1 // ver 3 | count changes
     public struct Receiver
     {
         private int count;
@@ -41,4 +52,16 @@ namespace Puzzle15_Value_Types_and_Event_Handlers
             EchoCount();
         }
     }
+
+    //ver 2 | Count changes
+    //public class Receiver
+    //{
+    //    private int count;
+    //    public void EchoCount() { count++; Console.WriteLine($"Current count: {count}"); }
+
+    //    public void TimerEvent(object sender, ElapsedEventArgs args)
+    //    {
+    //        EchoCount();
+    //    }
+    //}
 }
