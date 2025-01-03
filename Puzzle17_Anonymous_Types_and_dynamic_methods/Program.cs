1706d325df2b2bedf6e027888268a1f07f2fb5b0
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-01-02 18:26:20 -0700

    Set up Puzzle17_Creating_Dynamic_C#_Types puzzle

---
+++ Puzzle17_Anonymous_Types_and_dynamic_methods/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Puzzle17_Anonymous_Types_and_dynamic_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Puzzle17_Creating_Dynamic_C#_Types/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Dynamic;
+
+namespace Puzzle17_Creating_Dynamic_C__Types
+{
+    internal class Program
+    {
+        /*
+         > Gain a deeper understanding of the C# Runtime binder
+         > Understand how you can create dynamic objects in C#
+         
+         Original code results in: 
+         System.InvalidCastException: 
+         'Unable to cast object of type 'Puzzle17_Creating_Dynamic_C__Types.MyDynamicObject' 
+         to type 'System.Collections.IEnumerable'.'
+
+         KEY POINTS:
+         > 
+         */
+        static void Main(string[] args)
+        {
+            dynamic d = new MyDynamicObject();
+            Console.WriteLine(d == null);
+            var sequence = (IEnumerable)d;
+
+            Console.WriteLine(sequence == null);
+            foreach (var item in sequence)
+                Console.WriteLine(item);
+
+            foreach (var item in d)
+                Console.WriteLine(item);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class MyDynamicObject : DynamicObject
+    {
+        public override bool TryBinaryOperation(BinaryOperationBinder binder, object arg, out object? result)
+        {
+            Console.WriteLine($"TryBinaryOperation for {binder.Operation}");
+            result = "dynamically returned result";
+            return true;
+        }
+
+        public override bool TryConvert(ConvertBinder binder, out object? result)
+        {
+            if (binder.Type.Name.Contains("IEnumerable"))
+            {
+                result = new string[] { "dynamically", "returned", "result" };
+                return true;
+            }
+            else
+            {
+                result = null;
+                return false;
+            }
+        }
+    }
+}
